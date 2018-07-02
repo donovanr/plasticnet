ce1907293cf83d5db8f460961391bb6cc0da1912
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-02 14:59:32 -0700

    setting up travis

--- plastic_net/plastic_net.py
+++ plastic_net/plastic_net.py
@@ -1,20 +1,9 @@
 from __future__ import absolute_import, division, print_function
 import numpy as np
-import numba as nb
+import networkx as nx
 from numba import jit, jitclass, float32, int64
 
 __all__ = ["TimePoint"]
-
-
-# Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installed,
-# And calls this with duecredit (as in `python -m duecredit script.py`):
-due.cite(
-    Doi("10.1167/13.9.30"),
-    description="Template project for small scientific Python projects",
-    tags=["reference-implementation"],
-    path="plastic_net",
-)
 
 
 # TODO test whether math is faster than numpy
