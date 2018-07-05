14342f87dcaea82d1c079e2a83fe9e0d17540274
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-05 10:41:24 -0700

    simplify __init__.py

--- plastic_net/__init__.py
+++ plastic_net/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import, division, print_function
-from .version import __version__  # noqa
-from .plastic_net import *  # noqa
+from .version import __version__
+
+__all__ = ["__version__"]
