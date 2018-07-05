18d81a0a3e47c9fb4354d411d532aa1ae7979c63
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-05 11:27:22 -0700

    simplifying main init file

--- plastic_net/__init__.py
+++ plastic_net/__init__.py
@@ -1,7 +1,5 @@
-from __future__ import absolute_import, division, print_function
 from .version import __version__
-
 from .plastic_net import solve_gpnet
 from .utils import soft_thresh
 
-__all__ = ["__version__", "plastic_net", "utils", "soft_thresh", "solve_gpnet"]
+__all__ = ["__version__", "soft_thresh", "solve_gpnet"]
