5d57d24a0aaa251d0a421b9364dd3febf63a87cf
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-05 11:21:11 -0700

    local import

--- plastic_net/__init__.py
+++ plastic_net/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__
 
-from plastic_net import solve_gpnet
-from utils import soft_thresh
+from .plastic_net import solve_gpnet
+from .utils import soft_thresh
 
 __all__ = ["__version__", "plastic_net", "utils", "soft_thresh", "solve_gpnet"]
