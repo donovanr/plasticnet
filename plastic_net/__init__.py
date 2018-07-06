c9b4222c0608a8b0253e4acb55627df310d84929
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-06 11:04:28 -0700

    remove function promotion to main namespace

--- plastic_net/__init__.py
+++ plastic_net/__init__.py
@@ -1,5 +1,3 @@
 from .version import __version__
-from .plastic_net import solve_gpnet
-from .utils import soft_thresh
 
-__all__ = ["__version__", "soft_thresh", "solve_gpnet"]
+__all__ = ["__version__"]
