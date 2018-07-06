e425c72a23a8c7a9b551208603611c4e2a36efca
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-06 14:02:45 -0700

    added soft thresholding test

---
+++ plastic_net/tests/test_soft_thresh.py
@@ -0,0 +1,13 @@
+import numpy as np
+import numpy.testing as npt
+
+from plastic_net.utils.math import soft_thresh
+
+
+def test_soft_thresh():
+    """Soft thresholding tests"""
+
+    x = np.arange(-10, 11, dtype=float)
+    y = np.concatenate([np.arange(-9, 0), np.zeros(3), np.arange(1, 10)])
+    npt.assert_almost_equal(soft_thresh(1, x), y, decimal=10)
+    npt.assert_almost_equal(soft_thresh(np.max(x), x), np.zeros_like(x), decimal=10)
