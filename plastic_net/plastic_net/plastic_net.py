3de6ac4f6e4c61b206121ddc267b6ab7bb9cc459
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-06 15:12:50 -0700

    dosctring fuckery

--- plastic_net/plastic_net/plastic_net.py
+++ plastic_net/plastic_net/plastic_net.py
@@ -3,28 +3,7 @@ import numpy as np
 import networkx as nx
 from numba import jit, jitclass, float32, int64
 
-
-# TODO test whether math is faster than numpy
-@jit(nopython=True, nogil=True, cache=True)
-def soft_thresh(lam, x):
-    """
-    Soft thresholding operator.  Takes a numpy ndarray of floats and reutrns and element-wise soft-thesholded version.
-    Element-wise, the soft-thresholding operator :math:`S_\lambda(x)` is given by:
-    .. math::
-        \begin{equation*}
-            \S_\lambda(x) =
-                \begin{cases}
-                    x + \lambda, & \text{if $x < -\lambda$} \\
-                    0          , & \text{if $-\lambda < x < \lambda$} \\
-                    x - \lambda, & \text{if $x > \lambda$}
-                \end{cases}
-        \end{equation*},
-    where :math:`\lambda` is a scalar tresholding parameter.
-    Args:
-        lam (float): threshold value
-        x (ndarry): numpy array of floats to be soft thresholded
-    """
-    return np.sign(x) * np.maximum(np.abs(x) - lam, 0)
+from ..utils.math import soft_thresh
 
 
 @jit(nopython=True, nogil=True, cache=True)
