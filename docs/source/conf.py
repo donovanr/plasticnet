6b99c78cbae99053d1ea1df83fe8b248a7bc912f
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2018-07-09 14:25:30 -0700

    only keep correct sys.path mod

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,11 +20,7 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath("."))
-# sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../../"))
-# sys.path.insert(0, os.path.abspath("../../plasticnet"))
-# sys.path.insert(0, os.path.abspath("../../../"))
 
 
 # -- General configuration ------------------------------------------------
