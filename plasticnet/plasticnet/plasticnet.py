140a5b5337cdb8c5c3b193e0b9250a513c9e550b
Rory Donovan-Maiye <donovanr@alumni.reed.edu>
2018-07-13 12:04:59 -0700

    created in-place and non version of OLS and elastic net

--- plasticnet/plasticnet/plasticnet.py
+++ plasticnet/plasticnet/plasticnet.py
@@ -5,6 +5,42 @@ from ..utils import math
 
 
 @jit(nopython=True, nogil=True, cache=False)
+def _solve_ols_inplace(beta, r, X, thresh=1e-8, max_iters=100):
+    r"""
+    _solve_ols_inplace(beta, r, X, thresh=1e-8, max_iters=100)
+
+    Ordinary least squares regression.  This function finds the beta that minimizes
+
+    .. math::
+
+        \tfrac{1}{2N}||\vec{y}-X\cdot\vec{\beta}||_2^2
+
+    Args:
+        beta (numpy.ndarray): shape (D,) coefficient vector. modified in-place.
+        r (numpy.ndarray): shape (N,) residual, i.e :math:`\vec{r} = \vec{y} - X\vec{\beta}`. modified in-place.
+        X (numpy.ndarray): shape (N,D) data matrix.
+        y (numpy.ndarray): shape (N,) target vector.
+        thresh (float): convergence criterion for coordinate descent. coordinate descent runs until the maximum element-wise change in **beta** is less than **thresh**.
+        max_iters (int): maximum number of update passes through all P elements of **beta**, in case **thresh** is never met.
+
+    Note
+        **beta** and **residual** are modified in-place.  As inputs, if :math:`\beta = 0`, then it *must* be the case that :math:`r = y`, or the function will not converge tot he correct answer.
+    """
+
+    N, D = X.shape
+    rho = np.ones(D, dtype=np.float64) + thresh
+
+    iter_num = 0
+
+    while np.max(rho) > thresh and iter_num < max_iters:
+        iter_num += 1
+        for j in range(D):
+            rho[j] = np.dot(X[:, j], r) / N
+            r -= rho[j] * X[:, j]
+            beta[j] += rho[j]
+
+
+@jit(nopython=True, nogil=True, cache=False)
 def solve_ols(X, y, thresh=1e-8, max_iters=100):
     r"""
     solve_ols(X, y, thresh=1e-8, max_iters=100)
@@ -27,17 +63,60 @@ def solve_ols(X, y, thresh=1e-8, max_iters=100):
 
     N, D = X.shape
     beta = np.zeros(D, dtype=np.float64)
-    r = y.copy()
+    r = y - np.dot(X, beta)
+
+    _solve_ols_inplace(beta, r, X, thresh=thresh, max_iters=max_iters)
+
+    return beta
+
+
+@jit(nopython=True, nogil=True, cache=True)
+def _solve_enet_inplace(
+    beta, r, X, y, lambda_total=1.0, alpha=0.75, thresh=1e-8, max_iters=100
+):
+    r"""
+    _solve_enet_inplace(beta, r, X, y, lambda_total=1.0, alpha=0.75, thresh=1e-8, max_iters=100)
+
+    Elastic net regression.  This function finds the beta that minimizes
+
+    .. math::
+
+        \tfrac{1}{2N} ||\vec{y}-X\cdot\vec{\beta}||_2^2 + \lambda \bigl( \alpha||\vec{\beta}||_1 + (1-\alpha) \tfrac{1}{2} ||\vec{\beta}||_2^2 \bigr)
+
+    Args:
+        beta (numpy.ndarray): shape (D,) coefficient vector. modified in-place.
+        r (numpy.ndarray): shape (N,) residual, i.e :math:`\vec{r} = \vec{y} - X\vec{\beta}`. modified in-place.
+        X (numpy.ndarray): shape (N,D) data matrix.
+        y (numpy.ndarray): shape (N,) target vector.
+        lambda_total (float): must be non-negative. total regularization penalty strength.
+        alpha (float): mixing parameter between L1 and L1 penalties. must be between zero and one. :math:`\alpha=0` is pure L2 penalty, :math:`\alpha=1` is pure L1 penalty.
+        thresh (float): convergence criterion for coordinate descent. coordinate descent runs until the maximum element-wise change in **beta** is less than **thresh**.
+        max_iters (int): maximum number of update passes through all P elements of **beta**, in case **thresh** is never met.
+
+    Note
+        **beta** and **residual** are modified in-place.  As inputs, if :math:`\beta = 0`, then it *must* be the case that :math:`r = y`, or the function will not converge tot he correct answer.
+    """
+
+    lambda1 = alpha * lambda_total
+    lambda2 = (1.0 - alpha) * lambda_total
+
+    N, D = X.shape
+    beta_old = beta.copy()
+    delta_beta = np.ones(D, dtype=np.float64) + thresh
     rho = np.ones(D, dtype=np.float64) + thresh
 
     iter_num = 0
 
-    while np.max(rho) > thresh and iter_num < max_iters:
+    while np.max(delta_beta) > thresh and iter_num < max_iters:
         iter_num += 1
         for j in range(D):
-            rho[j] = np.dot(X[:, j], r) / N
-            r -= rho[j] * X[:, j]
-            beta[j] += rho[j]
+            rho[j] = np.dot(X[:, j], r)
+            beta[j] = math.soft_thresh(lambda1, beta_old[j] + rho[j] / N) / (
+                1 + lambda2
+            )
+            delta_beta[j] = beta[j] - beta_old[j]
+            r -= X[:, j] * delta_beta[j]
+            beta_old[j] = beta[j]
 
     return beta
 
@@ -62,28 +141,20 @@ def solve_enet(X, y, lambda_total=1.0, alpha=0.75, thresh=1e-8, max_iters=100):
         max_iters (int): maximum number of update passes through all P elements of **beta**, in case **thresh** is never met.
     """
 
-    lambda1 = alpha * lambda_total
-    lambda2 = (1.0 - alpha) * lambda_total
-
     N, D = X.shape
     beta = np.zeros(D, dtype=np.float64)
-    beta_old = np.zeros(D, dtype=np.float64)
-    delta_beta = np.ones(D, dtype=np.float64)
-    r = y.copy()
-    rho = np.ones(D, dtype=np.float64) + thresh
+    r = y - np.dot(X, beta)
 
-    iter_num = 0
-
-    while np.max(delta_beta) > thresh and iter_num < max_iters:
-        iter_num += 1
-        for j in range(D):
-            rho[j] = np.dot(X[:, j], r)
-            beta[j] = math.soft_thresh(lambda1, beta_old[j] + rho[j] / N) / (
-                1 + lambda2
-            )
-            delta_beta[j] = beta[j] - beta_old[j]
-            r -= X[:, j] * delta_beta[j]
-            beta_old[j] = beta[j]
+    _solve_enet_inplace(
+        beta,
+        r,
+        X,
+        y,
+        lambda_total=lambda_total,
+        alpha=alpha,
+        thresh=thresh,
+        max_iters=max_iters,
+    )
 
     return beta
 
