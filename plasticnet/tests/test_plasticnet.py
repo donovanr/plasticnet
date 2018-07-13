e016d1bba970e7c3aa91d4d716cccb880ad36c5c
Rory Donovan-Maiye <donovanr@alumni.reed.edu>
2018-07-13 13:15:50 -0700

    test elastic net on random alpha and lambda parameters

--- plasticnet/tests/test_plasticnet.py
+++ plasticnet/tests/test_plasticnet.py
@@ -1,4 +1,4 @@
-import numpy.testing as npt
+import numpy as np
 
 from sklearn import linear_model
 from sklearn.preprocessing import scale
@@ -20,10 +20,10 @@ def test_ols_explicit(N=200, D=100):
 
     beta = solve_ols(X, y, tol=1e-8, max_iter=1e3)
 
-    npt.assert_almost_equal(ols.coef_, beta, decimal=6)
+    np.testing.assert_almost_equal(ols.coef_, beta, decimal=6)
 
 
-def test_enet_ols(N=200, D=100):
+def test_enet_explicit_ols(N=200, D=100):
     """Test explicit lambda=0 elastic net in solve_enet against sklearn LinearRegression"""
 
     X, y, beta_true = make_regression(
@@ -36,20 +36,27 @@ def test_enet_ols(N=200, D=100):
 
     beta = solve_enet(X, y, lambda_total=0.0, alpha=0.0, tol=1e-8, max_iter=1e3)
 
-    npt.assert_almost_equal(ols.coef_, beta, decimal=6)
+    np.testing.assert_almost_equal(ols.coef_, beta, decimal=6)
 
 
-def test_enet_sklearn(N=200, D=100):
-    """General plastic net tests ElasticNet"""
+def test_enet_explicit(N=200, D=100):
+    """Test elastic net against sklearn ElasticNet"""
 
     X, y, beta_true = make_regression(
         n_samples=N, n_features=D, n_informative=N // 10, coef=True
     )
     X, y = scale(X), scale(y)
 
-    enet = linear_model.ElasticNet(alpha=0.5, l1_ratio=0.5, tol=1e-8, max_iter=1e3)
+    lambda_total = np.random.exponential()
+    alpha = np.random.rand()
+
+    enet = linear_model.ElasticNet(
+        alpha=lambda_total, l1_ratio=alpha, tol=1e-8, max_iter=1e3
+    )
     enet.fit(X, y)
 
-    beta = solve_enet(X, y, lambda_total=0.5, alpha=0.5, tol=1e-8, max_iter=1e3)
+    beta = solve_enet(
+        X, y, lambda_total=lambda_total, alpha=alpha, tol=1e-8, max_iter=1e3
+    )
 
-    npt.assert_almost_equal(enet.coef_, beta, decimal=6)
+    np.testing.assert_almost_equal(enet.coef_, beta, decimal=6)
